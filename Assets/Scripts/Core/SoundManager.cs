f90ccba724258a9bb7a7b7faa746178c2a993bb2
Mispon <tomythesupa@gmail.com>
2020-08-31 22:45:19 +0300

    Fixed clip checking

--- Assets/Scripts/Core/SoundManager.cs
+++ Assets/Scripts/Core/SoundManager.cs
@@ -67,7 +67,7 @@ namespace Core
         /// </summary>
         private void PlaySound(AudioClip clip)
         {
-            if (_noSound || clip is null)
+            if (_noSound || clip == null)
                 return;
             
             sfx.pitch = Random.Range(0.9f, 1.1f);
