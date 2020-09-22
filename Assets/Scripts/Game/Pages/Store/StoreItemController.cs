009fae00c935964e1b60fef97e15450e6e736278
Gr871 <ky3971@yandex.ru>
2020-09-22 11:35:17 +0300

    Strore page complete
    1. Добавлены и подключены все иконки
    2. Подгон авторазмеров иконок в магазине

--- Assets/Scripts/Game/Pages/Store/StoreItemController.cs
+++ Assets/Scripts/Game/Pages/Store/StoreItemController.cs
@@ -13,6 +13,12 @@ namespace Game.Pages.Store
     /// </summary>
     public class StoreItemController : MonoBehaviour
     {
+        [Header("Настройки слота иконки")]
+        [SerializeField] private RectTransform itemIconBackgroundRect;
+        [SerializeField] private RectTransform itemIconRect;
+        [SerializeField] private float marginPadding = 20f;
+        
+        [Header("Информация")]
         [SerializeField] private Image iconImg;
         [SerializeField] private Text typeTxt;
         [SerializeField] private Text priceTxt;
@@ -20,16 +26,34 @@ namespace Game.Pages.Store
         
         public GoodsType Type { get; private set; }
         public short Level { get; private set; }
+
+        private float _itemIconMaxSize;
         
         /// <summary>
         /// Инициализация UI-элементов
         /// </summary>
         public void Initialize(GoodsType type, GoodUI uiData, Action<GoodsType, short, int> onClickAction)
         {
+            if (_itemIconMaxSize == 0)
+                _itemIconMaxSize = itemIconBackgroundRect.rect.width - marginPadding;
+            
             Type = type;
             Level = uiData.Level;
             
             iconImg.sprite = uiData.Image;
+            
+            if (uiData.Image != null)
+            {
+                var texture = uiData.Image.texture;
+                var ratio =(float) texture.width / texture.height;
+                var scale = ratio >= 1f
+                    ? _itemIconMaxSize / texture.width
+                    : _itemIconMaxSize / texture.height;
+                
+                itemIconRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, texture.width * scale); 
+                itemIconRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, texture.height * scale); 
+            }
+            
             typeTxt.text = LocalizationManager.Instance.Get(type.GetDescription()).ToUpper();;
             priceTxt.text = uiData.Price.DisplayMoney();
             
