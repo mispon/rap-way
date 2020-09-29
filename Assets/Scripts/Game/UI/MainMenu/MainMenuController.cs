0e8e2b966a357537d9768e00f147849fb501cb28
Gr871 <ky3971@yandex.ru>
2020-09-29 14:56:37 +0300

    MainMenu controller
    1. Контроллер главного меню:
    1а. Отображение текущих метрик
    1б. Панель выбора окна перехода (новая игра, продолжить, настройки)
    2. Расширение функционала управления КанвасГруппой

--- Assets/Scripts/Game/UI/MainMenu/MainMenuController.cs
+++ Assets/Scripts/Game/UI/MainMenu/MainMenuController.cs
@@ -1,41 +1,101 @@
 ﻿using Core;
+using Core.Interfaces;
+using Models.UI;
 using UnityEngine;
+using UnityEngine.UI;
 using Utils;
+using Utils.Extensions;
 
 namespace Game.UI.MainMenu
 {
-    public class MainMenuController : MonoBehaviour
+    /// <summary>
+    /// Контроллер управления главным меню
+    /// </summary>
+    public class MainMenuController : Singleton<MainMenuController>, IStarter
     {
-        private const int NEW_GAME = 0;
-        private const int CONTINUE_GAME = 1;
-        private const int ABOUT = 2;
-        
-        [SerializeField] private GameObject newPlayerScreen;
-        [SerializeField] private Switcher menuSwitcher;
+        [Header("Metrics UI")] 
+        [SerializeField] private Text moneyText;
+        [SerializeField] private Text fansText;
+        [SerializeField] private Text hypeText;
 
-        private void Start()
+        [Header("Кнопки меню")] 
+        [SerializeField] private Button newGameButton;
+        [SerializeField] private Button continueGameButton;
+        [SerializeField] private Button settingsButton;
+        [SerializeField] private Button aboutButton;
+
+        [Header("Окна")] 
+        [SerializeField] private GameObject newPlayerPanel;
+        [SerializeField] private GameObject settingsPanel;
+        [SerializeField] private GameObject aboutPanel;
+        
+        [Header("CanvasGroup")] 
+        [SerializeField] private CanvasGroupController menuCanvasGroupController;
+        
+        [Header("Управление меню")]
+        [SerializeField] private Button menuButton;
+        [SerializeField] private Animator menuAnimator;
+        [SerializeField] private string showMenuTriggerName;
+        [SerializeField] private string hideMenuTriggerName;
+
+        private bool _menuState;
+        
+        public void OnStart()
         {
-            menuSwitcher.InstantiateElements(new[] {"New Game", "Continue", "About"});
-            menuSwitcher.AddClickCallback(OnItemSelect);
+            SetUpPlayerData();
+            SetUpButtons();
         }
 
-        private void OnItemSelect(int index)
+        /// <summary>
+        /// Устанавливает метрики текущего сохранения
+        /// </summary>
+        private void SetUpPlayerData()
         {
-            switch (index)
-            {
-                case NEW_GAME:
-                    newPlayerScreen.SetActive(true);
-                    break;
-                case CONTINUE_GAME:
-                    SceneManager.Instance.LoadGameScene();
-                    break;
-                case ABOUT:
-                    
-                    break;
-                default:
-                    throw new RapWayException("[StartMenuController] Действие не определено!");
-            }
-            gameObject.SetActive(false);
+            var playerInfo = GameManager.Instance.PlayerData;
+            moneyText.text = playerInfo.Money.DisplayShort();
+            fansText.text = playerInfo.Fans.DisplayShort();
+            hypeText.text = playerInfo.Hype.ToString();
+        }
+
+        /// <summary>
+        /// Создает обработчики нажатия на кнопки
+        /// </summary>
+        private void SetUpButtons()
+        {
+            newGameButton.onClick.AddListener(()=> ShowPanel(newPlayerPanel));
+            continueGameButton.onClick.AddListener(()=> SceneManager.Instance.LoadGameScene());
+            settingsButton.onClick.AddListener(()=> ShowPanel(settingsPanel));
+            aboutButton.onClick.AddListener(()=> ShowPanel(aboutPanel));
+            
+            menuButton.onClick.AddListener(ChangeMenuState);
+        }
+
+        /// <summary>
+        /// Открытие/закрытие меню
+        /// </summary>
+        private void ChangeMenuState()
+        {
+            menuAnimator.SetTrigger(_menuState ? hideMenuTriggerName : showMenuTriggerName);
+            _menuState = !_menuState;
+        }
+
+        /// <summary>
+        /// Переход к панели 
+        /// </summary>
+        private void ShowPanel(GameObject panel)
+        {
+            SetWindowActive(panel, true);
+            _menuState = true;
+            ChangeMenuState();
+        }
+        
+        /// <summary>
+        /// Открытие/закрытие окон главного меню
+        /// </summary>
+        public static void SetWindowActive(GameObject panel, bool value)
+        {
+            panel.SetActive(value);
+            Instance.menuCanvasGroupController.SetActive(!value);
         }
     }    
 }
--- Assets/Scripts/Utils/Extensions/IntExtension.cs
+++ Assets/Scripts/Utils/Extensions/IntExtension.cs
@@ -12,28 +12,36 @@ namespace Utils.Extensions
         {
             return value.ToString("C0", new CultureInfo("en-US"));
         }
+
+        /// <summary>
+        /// Функция представления денег в сокращенном формате
+        /// </summary>
+        public static string DisplayMoneyShort(this int value)
+        {
+            return $"${DisplayShort(value)}";
+        }
         
         /// <summary>
         /// Функция получения представления целых чисел в формате "##.#k"
         /// </summary>
-        public static string DisplayMoneyShort(this int value)
+        public static string DisplayShort(this int value)
         {
             if (value < 10e3)
                 return value.ToString();
 
             if (value < 10e6)
-                return DisplayShort(value, 3);
+                return GetShort(value, 3);
 
             if (value < 10e9)
-                return DisplayShort(value, 6);
+                return GetShort(value, 6);
 
-            return DisplayShort(value, 9);
+            return GetShort(value, 9);
         }
 
         /// <summary>
         /// Общая функция преобразования, оставляющая только 3 знака до запятой и нужное количестов "k"
         /// </summary>
-        private static string DisplayShort(int value, int dividerSize)
+        private static string GetShort(int value, int dividerSize)
         {
             var divider = Mathf.Pow(10, dividerSize);
             var dividedValue = value / divider;
